960ba36050c24ca0f086551d85c5c56ba1d293e1
filutfi <ltvie7s@gmail.com>
2017-06-07 14:29:25 +0700

    versi 3

--- cluadmin/config/db.php
+++ cluadmin/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=feunisma',
+    'dsn' => 'mysql:host=localhost;dbname=db_clutera',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
